117dacf967b6476fabe0e0cea67f82a08dba0311
Sanket Kshirsagar <sanketkshirsagar276@gmail.com>
2025-09-22 17:10:24 +0530

    Add files via upload

---
+++ sklearn1.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+iris = load_iris()
+
+X,y = iris.data,iris.target
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size=0.3, random_state=42)
+
+model = LogisticRegression(max_iter=200)
+model.fit(X_train,y_train)
+y_pred = model.predict(X_test)
+print('accuracy:',accuracy_score(y_pred,y_test))
