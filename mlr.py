d13aeb663e9b948f28335bfd6d5a5473cb45629e
Sanket Kshirsagar <sanketkshirsagar276@gmail.com>
2025-09-24 23:45:17 +0530

    Update mlr.py

--- mlr.py
+++ mlr.py
@@ -12,10 +12,6 @@ X = pd.get_dummies(X,dtype=int)
 from sklearn.model_selection import train_test_split
 X_train, X_test,y_train,y_test = train_test_split(X, y, test_size= 0.2, random_state = 0)
 
-from sklearn.linear_model import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=0)
-
-
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train,y_train)
@@ -64,3 +60,4 @@ bias
 
 variance = regressor.score(X_test, y_test)
 variance
+
